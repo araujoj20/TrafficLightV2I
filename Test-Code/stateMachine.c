5914148902de8805a68b8e657df57ba15abb8928
araujoj20 <jmcsa20@gmail.com>
2024-06-06 21:22:04 +0100

    state machine reformulated

--- Test-Code/stateMachine.c
+++ Test-Code/stateMachine.c
@@ -2,147 +2,158 @@
 #include <stdlib.h>
 #include <wiringPi.h>
 #include <wiringPiI2C.h>
-
 #include <stdbool.h>
 #include <signal.h>
 
-typedef enum{   
-    RED_LED         = 17, 
-    YELLOW_LED      = 27, 
-    GREEN_LED       = 22, 
-    PEDESTIAN_BTN   = 23,
-    EMERGENCY_BTN   = 24
-
+typedef enum {
+    LED_RED         = 17, 
+    LED_YELLOW      = 27, 
+    LED_GREEN       = 22, 
+    BTN_PEDESTRIAN  = 23,
+    BTN_EMERGENCY   = 24
 } LedPin;
 
 typedef enum {
     STATE_GREEN,
     STATE_YELLOW,
     STATE_RED,
-    NUM_STATES
+    STATE_EMERGENCY,
+    STATE_PEDESTRIAN,
+    STATES_NUMBER
 } State;
 
-typedef enum{   
-    EMERGENCY_TIME  = 10000,  //20000, 
-    PEDESTIAN_TIME  = 8000,  //15000, 
-    GREEN_TIME      = 5000,  //30000, 
-    YELLOW_TIME     = 1000,  //2000
-    RED_TIME        = 2000  //5000
-
+typedef enum {   
+    TIME_EMERGENCY  = 10000,
+    TIME_PEDESTRIAN = 8000, 
+    TIME_GREEN      = 5000,
+    TIME_YELLOW     = 1000,
+    TIME_RED        = 2000
 } Time;
 
+typedef struct {
+    State state;
+    Time time;
+    LedPin pin;
+    State (*nextStateFunction)();
+} Operation;
 
-typedef State (*StateFunction)();
-
-
-void lightLed(LedPin LedColor);
-
+void lightLed(LedPin ledColor);
 State greenState();
 State yellowState();
 State redState();
+State emergencyState();
+State pedestrianState();
 
-StateFunction stateFunctions[NUM_STATES] = {
-    greenState,
-    yellowState,
-    redState
+Operation operations[STATES_NUMBER] = {
+    {STATE_GREEN,       TIME_GREEN,      LED_GREEN,  greenState},
+    {STATE_YELLOW,      TIME_YELLOW,     LED_YELLOW, yellowState},
+    {STATE_RED,         TIME_RED,        LED_RED,    redState},
+    {STATE_EMERGENCY,   TIME_EMERGENCY,  LED_YELLOW, emergencyState},
+    {STATE_PEDESTRIAN,  TIME_PEDESTRIAN, LED_RED,    pedestrianState}
 };
 
 volatile bool pedestrianBtn = false;
 volatile bool emergencyBtn  = false;
-
-unsigned long lastTime = 0;
-volatile int waitTime       = GREEN_TIME;
-State currentState = STATE_GREEN;
-State nextState = STATE_GREEN;
+const unsigned int debounceDelay = 50;
 
 void pedestrianISR(void) {
-    pedestrianBtn = true;
-    delay(50);
+    static unsigned int lastPedestrianPress = 0;
+    unsigned int currentTime = millis();
+    if (currentTime - lastPedestrianPress > debounceDelay) {
+        pedestrianBtn = true;
+        lastPedestrianPress = currentTime;
+    }
 }
 
 void emergencyISR(void) {
-    emergencyBtn = true;
-    delay(50);
+    static unsigned int lastEmergencyPress = 0;
+    unsigned int currentTime = millis();
+    if (currentTime - lastEmergencyPress > debounceDelay) {
+        emergencyBtn = true;
+        lastEmergencyPress = currentTime;
+    }
 }
 
 void closeProg(int signal) {
-    digitalWrite(RED_LED, LOW);
-    digitalWrite(YELLOW_LED, LOW);
-    digitalWrite(GREEN_LED, LOW);
-    
+    digitalWrite(LED_RED, LOW);
+    digitalWrite(LED_YELLOW, LOW);
+    digitalWrite(LED_GREEN, LOW);
     exit(0);
 }
 
 void setup() {
     wiringPiSetupGpio();
-    /* LEDS */
-    pinMode(GREEN_LED, OUTPUT);
-    pinMode(YELLOW_LED, OUTPUT);
-    pinMode(RED_LED, OUTPUT);
-    /* BUTTONS */
-    pinMode(PEDESTIAN_BTN, INPUT);
-    wiringPiISR(PEDESTIAN_BTN, INT_EDGE_RISING, &pedestrianISR);
-    pinMode(EMERGENCY_BTN, INPUT);
-    wiringPiISR(EMERGENCY_BTN, INT_EDGE_RISING, &emergencyISR);
-    /* SIGNALS */
+    pinMode(LED_GREEN,  OUTPUT);
+    pinMode(LED_YELLOW, OUTPUT);
+    pinMode(LED_RED,    OUTPUT);
+    pinMode(BTN_PEDESTRIAN, INPUT);
+    pinMode(BTN_EMERGENCY,  INPUT);
+    wiringPiISR(BTN_PEDESTRIAN, INT_EDGE_RISING, &pedestrianISR);
+    wiringPiISR(BTN_EMERGENCY,  INT_EDGE_RISING, &emergencyISR);
     signal(SIGINT, closeProg);
 }
 
-State greenState() {
-    lightLed(GREEN_LED);
+void lightLed(LedPin ledColor) {
+    digitalWrite(LED_RED, (ledColor == LED_RED) ? HIGH : LOW);
+    digitalWrite(LED_YELLOW, (ledColor == LED_YELLOW) ? HIGH : LOW);
+    digitalWrite(LED_GREEN, (ledColor == LED_GREEN) ? HIGH : LOW);
+}
 
-    waitTime = GREEN_TIME;
-    nextState = STATE_YELLOW;
+State greenState() {
+    lightLed(LED_GREEN);
     return STATE_YELLOW;
 }
 
 State yellowState() {
-    lightLed(YELLOW_LED);
-
-    waitTime = YELLOW_TIME;
-    nextState = STATE_RED;
+    lightLed(LED_YELLOW);
     return STATE_RED;
 }
 
 State redState() {
-    lightLed(RED_LED);
-
-    waitTime = RED_TIME;
-    nextState = STATE_GREEN;
+    lightLed(LED_RED);
     return STATE_GREEN;
 }
 
-void lightLed(LedPin LedColor) {
-    digitalWrite(RED_LED,    (LedColor == RED_LED)    ? HIGH : LOW);
-    digitalWrite(YELLOW_LED, (LedColor == YELLOW_LED) ? HIGH : LOW);
-    digitalWrite(GREEN_LED,  (LedColor == GREEN_LED)  ? HIGH : LOW);
+State emergencyState() {
+    lightLed(LED_YELLOW);
+    return STATE_RED;
+}
+
+State pedestrianState() {
+    lightLed(LED_RED);
+    return STATE_GREEN;
 }
 
 int main(void) {
     setup();
-    unsigned long currentTime = 0;
+    unsigned char f_priority = 0;
+    unsigned int currentTime = 0;
+    unsigned int lastTime = 0;
+    unsigned int waitTime = 0;
+    State currentState  = STATE_GREEN;
+    State nextState     = STATE_GREEN;
+
     while (1) {
         currentTime = millis();
 
         if (pedestrianBtn) {
-            currentState = STATE_RED;
-            currentState = stateFunctions[currentState]();
+            currentState = STATE_PEDESTRIAN;
             pedestrianBtn = false;
-            lastTime = currentTime;
-            waitTime = PEDESTIAN_TIME;
+            f_priority = 1;
         }
 
         if (emergencyBtn) {
-            currentState = STATE_YELLOW;
-            currentState = stateFunctions[currentState]();
+            currentState = STATE_EMERGENCY;
             emergencyBtn = false;
-            lastTime = currentTime;
-            waitTime = EMERGENCY_TIME;
+            f_priority = 1;
         }
-        
-        if (currentTime - lastTime >= waitTime) {
-            currentState = stateFunctions[currentState]();
-            lastTime = currentTime;
+
+        if ((currentTime - lastTime >= waitTime) || f_priority) {
+            nextState = operations[currentState].nextStateFunction();
+            waitTime  = operations[currentState].time;
+            lastTime  = currentTime;
+            currentState = nextState;
+            f_priority = 0;
         }
 
         delay(100);
